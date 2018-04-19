16279671144a0362ce02d5f7c73d6a28a334afcd
swapnakathula <kathulaswapna@gmail.com>
2018-04-19 13:45:34 -0500

    some progress

---
+++ models/alert.js
@@ -0,0 +1,38 @@
+
+module.exports = function(sequelize, DataTypes) {
+    var Alert = sequelize.define("Alert", {
+      text:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+         notEmpty: true
+                        
+      },
+      alertType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+                  
+        },
+        destination: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [10,50],
+            notEmpty: true
+                      
+          },
+        });
+
+  Alert.associate = function(models) {
+    
+    Alert.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Alert;
+};
