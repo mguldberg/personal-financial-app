3274cf07fa51db4dd9e75c7681aaaeee18cb6a2b
mguldberg <guldberg@sbcglobal.net>
2018-04-23 00:29:03 -0500

    finished crypto investment API and writing to DB.  Also validity check for crypto currency lookup working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,28 @@
+//express NPM module and .Router to route front end AJAX requests 
 var express = require("express");
 var router = express.Router();
 
-// Requiring our models
+//cryptocompare and node-fetch NPM modules for getting crypto quotes
+global.fetch = require('node-fetch')
+const cc = require('cryptocompare')
+
+//moment NPM module
+var moment = require('moment');
+
+// Requiring our DB models
 var db = require("../models");
 
+//global var that will be run once on init and then available for looking up crypto currency
+var coinListObj = {};
+
+//
 // Create all our routes and set up logic within those routes where required.
+//
 
 // Handles adding user to DB
 router.post("/api/users/", function (req, res) {
 
-    console.log("in create User handler");
+    console.log("in create User handler - /api/users/ - POST");
     console.log(req.body)
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
@@ -41,7 +54,7 @@ router.post("/api/users/", function (req, res) {
 // signin attempt of user
 router.get("/api/users", function (req, res) {
 
-    console.log("in create User handler");
+    console.log("in signin attempt User handler - /api/users - GET");
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
@@ -49,8 +62,8 @@ router.get("/api/users", function (req, res) {
     db.User.findOne({
         where: {
             userName: req.query.username,
-           $and:[{
-            password: req.query.password
+            $and: [{
+                password: req.query.password
             }]
         }
 
@@ -75,7 +88,7 @@ router.get("/api/users", function (req, res) {
 // get expenses api
 router.get("/api/expenses/:id", function (req, res) {
 
-    console.log("in GET expenses handler");
+    console.log("in GET expenses handler - /api/expenses/:id - GET");
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
@@ -99,7 +112,7 @@ router.get("/api/expenses/:id", function (req, res) {
 // Handles adding expense to DB
 router.post("/api/expenses/:id", function (req, res) {
 
-    console.log("in POST expenses handler");
+    console.log("in POST expenses handler - /api/expenses/:id - POST");
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
@@ -125,36 +138,195 @@ router.post("/api/expenses/:id", function (req, res) {
 });
 
 // Handles adding user to DB
-router.post("/api/invenstment/", function (req, res) {
+router.post("/api/investment/:id", function (req, res) {
 
-    console.log("in create User handler");
-    console.log(req.body)
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
-    db.User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        userName: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        cellPhone: req.body.phone,
-        carrier: req.body.carrier,
-    }).then(function (dbUserResp) {
+    console.log("inside Investment POST handler - /api/investment/:id - POST")
 
+    switch (req.body.type) {
+        //is this investment a Crypto Currency
+        case 'Crypto Currency':
+            //           
+            // first check to see if Crypto name is valid
+            //
+
+            //initialize that we have not found a match before search loop
+            var coinMatchBool = false;
+
+            //strip out * from string in current index - will cause a crash if not handled
+            regexString = req.body.investmentName.replace(/\*/g, "");
+            console.log(regexString);
+
+            //construct a regex string to look for in the coinList
+            var regexVar = new RegExp(regexString, "gi");
+
+            console.log("regexvar", regexVar)
+            //loop through coinListObj set up when code first ran
+            for (i in coinListObj.Data) {
+
+                //use the RegExp .test function - returns true if a match is found
+                coinMatchBool = regexVar.test(coinListObj.Data[i].FullName)
+
+                //if a match was found
+                //  - reset req.body.investmentName to proper symbol
+                //  - set Image to store in DB for display by the Front End
+                if (coinMatchBool == true) {
+                    console.log("found a match");
+
+                    //reset investmentName to proper symbol for doing API lookup of price, etc.
+                    req.body.investmentName = coinListObj.Data[i].Symbol;
+
+                    //set image URL for use in Front End Display
+                    //"https://www.cryptocompare.com" + coinListObj.Data[i].ImageUrl
+                    req.body.investmentImgUrl = coinListObj.Data[i].ImageUrl;
+                    console.log(req.body.investmentImgUrl);
+
+                    //stop looping we found a match
+                    break;
+                }
+            }
+
+            //if we didn't find a match - inform the user of the problem
+            //leave immediately from this route and return error code to front end
+            if (coinMatchBool == false) {
+                console.log("invalid req.body.investmentName in investments POST handler");
+                res.status(500).send({ msg: "Invalid CryptoCoin entered.  Please try again." });
+            }
+            console.log(req.body.investmentName);
+
+            //API call requires an array of currencies
+            var currencyArray = [req.body.investmentName];
+
+            req.body.datePurchased = moment(req.body.datePurchased, 'MM/DD/YYYY').format('YYYY-MM-DD');
+
+            console.log("date purchased ", req.body.datePurchased)
+
+            // make API call to get current Stock value
+            cc.priceFull(currencyArray, ['USD'])
+                .then(prices => {
+                    // -> { BTC: { USD: 1114.63 } }
+                    console.log("cc.priceFull");
+                    console.log(prices);
+                    console.log(prices[req.body.investmentName].USD.PRICE);
+                    req.body.currentValue = parseFloat(prices[req.body.investmentName].USD.PRICE) * req.body.amount;
+                    console.log("current value of ", req.body.investmentName, " is ", req.body.currentValue);
+
+                    //checking to see if the optional parameter for Cost Basis was sent
+                    if (req.body.costBasis == 0) {
+                        // make API call to get costBasis value
+                        cc.priceHistorical(req.body.investmentName, ['USD'], new Date(req.body.datePurchased))
+                            .then(cryptoHistoricalResponse => {
+                                // pricesresponse format
+                                // -> { BTC: { USD: 997, EUR: 948.17 } }
+                                console.log("cc.priceHistorical");
+                                console.log(cryptoHistoricalResponse);
+                                console.log(cryptoHistoricalResponse.USD);
+
+                                //set the costBasis = amount of the coin * price of the coin at the time of purchase
+                                req.body.costBasis = req.body.amount * cryptoHistoricalResponse.USD
+
+                                console.log(req.body.costBasis);
+
+                                //make function call to add investment to the DB
+                                addInvestment(req.params, req.body, coinListObj.Data[req.body.investmentName].ImageUrl, res);
+
+                            })
+                    }
+                    else
+                        //make function call to add investment to the DB
+                        addInvestment(req.params, req.body, coinListObj.Data[req.body.investmentName].ImageUrl, res);
+
+                })
+                .catch(console.error);
+
+            break;
+
+        //is investment a Stock
+        case 'Stock':
+            //checking to see if the optional parameter for Cost Basis was sent
+            if (req.body.costBasis == 0) {
+                // call historic Stock price lookup api using Yahoo-finance API
+
+            }
+
+            // call Stock price lookup api using Yahoo-finance API
+
+            break;
+
+        default:
+            console.log("invalid req.body.type in investments POST handler");
+            res.status(500).send({ msg: "invalid req.body.type in investments POST handler" });
+
+    };
+
+});
+
+//api route for getting all of the investments
+router.get("/api/investment/:id", function (req, res) {
+    db.User.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.Investment]
+    }).then(function (dbInvestments) {
+        res.json(dbInvestments);
+    });
+});
+
+function addInvestment(params, body, imgUrl, res) {
+    // INSERT new row into the Investments table using the UserID key
+    db.Investment.create({
+        UserId: params.id,
+        type: body.type,
+        investmentName: body.investmentName,
+        amount: body.amount,
+        datePurchased: body.datePurchased,
+        costBasis: body.costBasis,
+        currentValue: body.currentValue,
+        investmentImgUrl: imgUrl
+    }).then(function (dbInvestmentResp) {
         // We have access to the new todo as an argument inside of the callback function
-        res.json(JSON.stringify(dbUserResp));
+        res.json(JSON.stringify(dbInvestmentResp));
 
     })
-        .catch(function (dbUserResp) {
-            console.log("we got an error", dbUserResp);
+        .catch(function (dbInvestmentResp) {
+            console.log("we got an error", dbInvestmentResp);
             // Whenever a validation or flag fails, an error is thrown
             // We can "catch" the error to prevent it from being "thrown", 
             // which could crash our node app
-            res.status(500).send(dbUserResp);
-
+            res.status(500).send(dbInvestmentResp);
         });
-});
+}
+
+
+//API call to get DB of valid coins
+cc.coinList()
+    .then(coinList => {
+        console.log("cc.coinList");
+        // console.log(coinListObj)
+        // ->
+        // {
+        //   BTC: {
+        //    Id: "1182",
+        //    Url: "/coins/btc/overview",
+        //    ImageUrl: "/media/19633/btc.png",
+        //    Name: "BTC",
+        //    Symbol: "BTC",
+        //    CoinName: "Bitcoin",
+        //    FullName: "Bitcoin (BTC)",
+        //    Algorithm: "SHA256",
+        //    ProofType: "PoW",
+        //    FullyPremined: "0",
+        //    TotalCoinSupply: "21000000",
+        //    PreMinedValue: "N/A",
+        //    TotalCoinsFreeFloat: "N/A",
+        //    SortOrder: "1",
+        //    Sponsored: false
+        // },
+        //   ETH: {...},
+        // }
+        coinListObj = coinList;
+    })
+    .catch(console.error)
 
 // Export routes for server.js to use.
 module.exports = router;
