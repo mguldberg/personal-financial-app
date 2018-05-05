6c5c402e516c188eb506aed62419d991da2df3e9
Eric Trautmiller <trautmillereahs@gmail.com>
2018-05-04 22:13:27 -0500

    test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -408,11 +408,11 @@ router.post("/api/investment/:id", function (req, expressRes) {
                             }, function (err, quotes) {
                               if (!quotes || quotes.length<1){
                                   console.log("QUOTES UNDEFINED")
-                                expressRes.status(404).send({ msg: "Invalid Stock entered.  Please try again." });   
+                                expressRes.status(404).send({ msg: "Error finding historical data.  Please try again." });   
                               }
                                 if (quotes[0].adjClose==undefined) {
                                     console.log("invalid req.body.investmentName in investments POST handler");
-                                    expressRes.status(404).send({ msg: "Invalid Stock entered.  Please try again." });
+                                    expressRes.status(404).send({ msg: "Error finding historical data.  Please try again." });
                                 }
                                 console.log(quotes)
                                 console.log(quotes[0].adjClose)
